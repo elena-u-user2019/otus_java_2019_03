1e2a820145c0f1a4a5935f97cea95216177acce9
Elena Utemisheva(Елена Утемишева) <elena.u.utemisheva@gmail.com>
2019-04-24 22:52:45 +0300

    Работа над ошибками HW_1

---
+++ hw01-maven/src/main/java/ru/otus/homework/HelloOtus.java
@@ -0,0 +1,16 @@
+package ru.otus.homework;
+
+import static org.apache.commons.lang3.ArrayUtils.add;
+
+public class HelloOtus {
+    public static void main(String[] args)
+    {
+        String[] phrase = {"Hello"};
+        phrase = add(phrase, " ");
+        phrase = add(phrase, "world");
+        for (String word : phrase) {
+            System.out.print(word);
+        }
+        System.out.println();
+    }
+}
